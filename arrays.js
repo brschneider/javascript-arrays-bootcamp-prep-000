32f9b7e33f9b33c1384aa74348dbfb373455d1d4
Brandon <brschneider@gmail.com>
2018-06-11 06:23:31 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(a, b) {
+ var a = [1, 2, 3, 4]
+ return a.unshift(5)
+}
+
+function destructivelyAddElementToBeginningOfArray(a, b) {
+  
+}
